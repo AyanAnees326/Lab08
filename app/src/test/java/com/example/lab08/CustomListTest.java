69b2ebc35734ef9578f400a5c5bf99fa76a499e0
AyanAnees326 <ayananees326@gmail.com>
2026-03-19 12:22:00 +0500

    failing test for hasCity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,7 +1,21 @@
 package com.example.lab08;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+        CustomList customList = new CustomList();
+
+        City city1 = new City("Lahore", "Punjab");
+        City city2 = new City("Karachi", "Sindh");
+
+        customList.addCity(city1);
+
+        assertTrue(customList.hasCity(city1));
+        assertFalse(customList.hasCity(city2));
+    }
 }
